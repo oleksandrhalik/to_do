46c3979f113634b426c0e8e9eeb63fee0b2a3a94
Oleksandr <oleksandr.halik@gmail.com>
2020-12-16 20:11:18 +0200

    Updates

---
+++ precache-manifest.1ea6fdbd4511e93a7b82986af85711ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "994df56a8236010a2a492ef8673baefc",
+    "url": "./index.html"
+  },
+  {
+    "revision": "70118f760d8407cc2e07",
+    "url": "./static/css/main.e3619f84.chunk.css"
+  },
+  {
+    "revision": "0aba61f0cc2ccbf514ed",
+    "url": "./static/js/2.44079c43.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "./static/js/2.44079c43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70118f760d8407cc2e07",
+    "url": "./static/js/main.61c2fbe1.chunk.js"
+  },
+  {
+    "revision": "35878c0637cea8221062",
+    "url": "./static/js/runtime-main.ba67aef9.js"
+  }
+]);
